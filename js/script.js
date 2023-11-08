2371b9b22f0d5049b640a7e41c6758384824a6f5
ETGxShaggyx <143112783+ETGxShaggyx@users.noreply.github.com>
2023-11-08 10:38:14 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Basic JavaScript Event Handling and DOM</title>
-  <link rel="stylesheet" href="style/style.css">
-  <script async src="js/script.js"></script>
-</head>
-<body>
-  <h1 class="center">ITMD-361 Basic JavaScript Event Handling and DOM</h1>
-  <h2 class="center">Anthony M Tetrev</h2>
-  <h3 class="center">atetrev1@hawk.itt.edu</h3>
-  <div id="container">
-    <input id="entryinput" type="text" name="textentry" placeholder="Enter a Message">
-    <div id="entrybutton">Alert Me</div>
-    <h2 id="textoutput" class="center">Show results here</h2>
-  </div>
-</body>
-</html>
+function init(){
+  var button = document.getElementById('entrybutton');
+
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Daniel Krieglstein: " + textbox.value);
+  }
+
+  button.addEventListener('click', showMeText);
+  }
+  
+window.addEventListener('load', init);
